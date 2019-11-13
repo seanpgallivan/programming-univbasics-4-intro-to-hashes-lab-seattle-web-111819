c9331d2e17377f7ae15121bb59f1483a3cdef91c
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-13 07:30:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,10 @@ def id_generator
   {id: 1}
 end
 def my_hash_creator(key, value)
-  {key => value
+  {key => value}
 end
-
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
